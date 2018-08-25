e68da35a4752962e682e3db45b4fab71f4cb163f
justinPCM <jpmmanalili@gmail.com>
2018-08-25 10:30:20 +0800

    Created layout for displaying list of categories

--- resources/views/categories/list.blade.php
+++ resources/views/categories/list.blade.php
@@ -1,20 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    CATEGORY LIST
+@section('content')
+<div class="container profile-margin">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h4 class="activities-header">Category List</h4>
+            <hr class="custom-hr">
+
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 list-container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4>Category Name</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-4 pull-right">
+                            <form action="#" method="POST">
+                                <input class="learn-button" type="submit" class="btn btn-sm" value="LEARN">
+                            </form>
+                        </div>
+                    </div>
                 </div>
+            </div>
 
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 list-container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4>Category Name</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-4 pull-right">
+                            <form action="#" method="POST">
+                                <input class="learn-button" type="submit" class="btn btn-sm" value="LEARN">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 list-container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4>Category Name</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-4 pull-right">
+                            <form action="#" method="POST">
+                                <input class="learn-button" type="submit" class="btn btn-sm" value="LEARN">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 list-container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <h4>Category Name</h4>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-8">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
+                        <div class="col-md-4 pull-right">
+                            <form action="#" method="POST">
+                                <input class="learn-button" type="submit" class="btn btn-sm" value="LEARN">
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
