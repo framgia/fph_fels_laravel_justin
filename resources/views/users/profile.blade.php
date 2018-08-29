01a98791fa867ff26a6c04bbfcb569c383586362
justinPCM <jpmmanalili@gmail.com>
2018-08-29 15:53:24 +0800

    Created layout for profile page

---
+++ resources/views/users/profile.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container profile-margin">
+    <div class="row">
+        <div class="col-md-4">
+            <img class="user-image" src="/uploads/avatars/{{ $user->avatar }}">            
+            <h4 class="text-center">{{ $user->name }}</h4>
+            <hr class="custom-hr">
+           <!-- <form enctype="multipart/form-data" action="/profile" method="POST">
+               <input type="file" name="avatar">
+               <input type="hidden" name="_token" value="{{ csrf_token() }}">
+               <input type="submit" class="pull-right btn btn-sm btn-primary">
+           </form> -->
+            <div class="row">
+                <div class="col-md-4 col-md-offset-2 text-center">
+                    <p>50<br/>followers</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <p>20<br/>following</p>
+                </div>
+            </div>
+
+            <div class="row text-center">
+                <form action="/profile" method="POST">
+                    <input class="follow-button" type="submit" class="btn btn-sm" value="FOLLOW">
+                </form>
+            </div>
+
+            <div class="row text-center">
+                <a href="#">Learned 20 Words</a>
+            </div>
+        </div>
+
+        <div class="col-md-7 activities">
+            <h4 class="activities-header">Activities</h4>
+            <hr class="custom-hr">
+            <div class="row">
+                <div class="col-md-2">
+                    <img class="user-image-sm" src="/uploads/avatars/default.jpg">  
+                </div>
+                <div class="col-md-10">
+                    <p class="activities-text"><a href="#">Jane</a> learned 20 of 20 words in <a href="#">Basic 500</a><br/>2 days ago</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <img class="user-image-sm" src="/uploads/avatars/default.jpg">  
+                </div>
+                <div class="col-md-10">
+                    <p class="activities-text"><a href="#">Jane</a> learned 20 of 20 words in <a href="#">Basic 500</a><br/>2 days ago</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <img class="user-image-sm" src="/uploads/avatars/default.jpg">  
+                </div>
+                <div class="col-md-10">
+                    <p class="activities-text"><a href="#">Jane</a> learned 20 of 20 words in <a href="#">Basic 500</a><br/>2 days ago</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <img class="user-image-sm" src="/uploads/avatars/default.jpg">  
+                </div>
+                <div class="col-md-10">
+                    <p class="activities-text"><a href="#">Jane</a> learned 20 of 20 words in <a href="#">Basic 500</a><br/>2 days ago</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
